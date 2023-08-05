0e97eebb05e407c5e1c8748766637facadba27d6
Dan <dan@howtobearealprogrammer.com>
2023-08-05 20:57:27 +1200

    adding vanta.js fog

---
+++ app.js
@@ -0,0 +1,12 @@
+VANTA.FOG({
+  el: "#vanta-fog",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  highlightColor: 0x2f2200,
+  lowlightColor: 0x11008e,
+  baseColor: 0x0,
+  zoom: 1.4,
+});
