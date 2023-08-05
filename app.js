c90392c958733a05f53a1279ca4a95e28364ccbf
Dan <dan@howtobearealprogrammer.com>
2023-08-05 21:28:01 +1200

    change to dots

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ VANTA.FOG({
   baseColor: 0x0,
   zoom: 1.4,
 });
+VANTA.DOTS({
+  el: "#vanta-dots",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  scale: 1.0,
+  scaleMobile: 1.0,
+});
